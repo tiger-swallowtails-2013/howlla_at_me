6cfa1efd752a54fb09881f1788a0b5eacfc1eba5
Apprentice <apprentice@dbc-23.local>
2013-10-07 14:14:20 -0700

    generate new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Howlla::Application.config.secret_token = 'cac9a3577b453b360fcc24a9433d5c0a760acb50a6eedb4fb2bb6d23252c53039f61bb6ecb83887d71e20195580f0010a18e4593a2eb170bdd299b7c850a335a'
