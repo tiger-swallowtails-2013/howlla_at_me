521dd0b4b73e57e56c22de2a1c77b8ad0483c93a
Apprentice <apprentice@dbc-23.local>
2013-10-07 20:46:56 -0700

    define create method for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   def new
+    @user =  User.new
+  end
+
+  def create
+    @user = User.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password])
+
+    @user.save
   end
 end
